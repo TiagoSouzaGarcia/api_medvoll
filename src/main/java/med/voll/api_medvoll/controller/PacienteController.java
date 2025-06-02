24e7c9a11470a13ca578a83a5a0e780504131a76
Tiago Garcia <tiago.garcia@asq.com.br>
2025-06-01 22:02:18 -0300

    feat: listagem paciente

--- src/main/java/med/voll/api_medvoll/controller/PacienteController.java
+++ src/main/java/med/voll/api_medvoll/controller/PacienteController.java
@@ -2,9 +2,13 @@ package med.voll.api_medvoll.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api_medvoll.paciente.DadosCadastroPaciente;
+import med.voll.api_medvoll.paciente.DadosListagemPaciente;
 import med.voll.api_medvoll.paciente.Paciente;
 import med.voll.api_medvoll.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,14 +19,14 @@ public class PacienteController {
     @Autowired
     private PacienteRepository repository;
 
-    @GetMapping
-    public void olaPacientes() {
-        System.out.println("Olá pacientes!");
-    }
-
     @PostMapping("/save")
     @Transactional
     public void save(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> list(@PageableDefault(size = 10, sort = "nome")Pageable page) {
+        return repository.findAll(page).map(DadosListagemPaciente::new);
     }
 }
--- src/main/java/med/voll/api_medvoll/repository/MedicoRepository.java
+++ src/main/java/med/voll/api_medvoll/repository/MedicoRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long>{
-
-
 }
